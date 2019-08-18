34bd917ecb36e7477460d83e60396dd50b1a734d
KKodiac <seanhong2000@gmail.com>
2019-08-18 15:10:02 +0900

    Adding Web

--- Crawler/WikiCrawler.py
+++ Crawler/WikiCrawler.py
@@ -1,6 +1,6 @@
 import nltk
-from pathlib import Path 
-import nltk 
+from pathlib import Path
+import nltk
 import requests
 from bs4 import BeautifulSoup as bs4
 from os import mkdir
@@ -11,16 +11,20 @@ import re
 test_correct_url_ = "https://en.wikipedia.org/wiki/Wiki"
 test_wrong_url_ = "https://en.wcor.org/wiki/Wiki"
 
-dir ="./DataFile/"
+dir = "./DataFile/"
+
 
 class Checker:
-    def __init__(self, DIR=dir,filename=""):
+    def __init__(self, DIR=dir, filename=""):
         self.url = []
         self.DIR = DIR
         self.filename = filename
         self.FILEPATH = self.DIR + self.filename
+
     def checkReqPackage(self):
-        requirements=['punkt', 'universal_tagset', 'averaged_perceptron_tagger']
+        requirements = [
+            'punkt', 'universal_tagset', 'averaged_perceptron_tagger'
+        ]
         pack = nltk.downloader.Downloader()
         for mod in requirements:
             if(pack.is_installed(mod)):
@@ -31,7 +35,7 @@ class Checker:
                 pack.download(info_or_id=mod)
                 print("Download complete!\n")
         print("All SET!\n")
-            
+
     def checkFilePath(self):
         try:
             mkdir(self.DIR)
@@ -42,16 +46,17 @@ class Checker:
         else:
             new_file = open(self.FILEPATH, 'w+')
             new_file.close()
-        
+
 
 class Crawler:
     def __init__(self, topic=""):
-        self.fileURL = dir+topic+".json"
+        self.fileURL = dir + topic + ".json"
         self.topic = topic
         self.wiki_path = "https://en.wikipedia.org/wiki/"
         self.WIKILINK = self.wiki_path + self.topic
         self.parags = []
         self.topicList = list()
+
     def requestForHTML(self):
         try:
             url = requests.get(self.WIKILINK)
@@ -67,16 +72,22 @@ class Crawler:
         parags = soupify.find(id="mw-content-text").find(class_="mw-parser-output").find_all("p", recursive=False)
         # print(parags)
         for link in parags:
-            if(link!=None):
-               self.topicList.append(link.find_all('a'))
+            if(link is not None):
+                self.topicList.append(link.find_all('a'))
         tag_file = open(self.fileURL, 'a', encoding='utf-8')
+        tag_file.write('[\n')
         for i in self.topicList:
             for a in i:
-                if(re.match('/wiki/*', a.attrs['href'])!=None):
-                    json.dump(a.attrs, tag_file)
+                if(re.match('/wiki/*', a.attrs['href']) is not None):
+                    data = {
+                        'title': a.attrs['title'],
+                        'link': a.attrs['href']
+                    }
+                    json.dump(data, tag_file, indent=3)
+                    tag_file.write(',\n')
+        tag_file.write('{}\n]')
         tag_file.close()
-                    
-        
+
 class Parser:
     pass
-    
+
