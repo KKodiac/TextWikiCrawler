5341e441960496f6aaacce6a560775ec9bdb5ff3
Kodiac <seanhong2000@gmail.com>
2019-09-04 23:53:12 +0900

    .

--- Web/notes/urls.py
+++ Web/notes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('<int:id>/', views.index, name='index'),
+    path('<int:id>/results', views.results, name='results')
 ]
--- Web/notes/views.py
+++ Web/notes/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Topic
 
 # Create your views here.
-def index(request):
-    pass
+def index(request, id):
+    return HttpResponse("You're looking at the main page for TOPIC %s" % id)
+
+def results(request, id):
+    show_topic = Topic.objects.all()
+    template = loader.get_template('notes/results.html')
+    context = {
+        'show_topic': show_topic,
+    }
+    
+    return HttpResponse(template.render(context, request))
+
