31aab67ebedc78f9d7c863f06001222b695e73dd
Kodiac <seanhong2000@gmail.com>
2019-12-01 00:01:28 +0900

    Shows frequently listed words

--- Crawler/processor.py
+++ Crawler/processor.py
@@ -1,4 +1,6 @@
 import nltk
+import matplotlib.pyplot as plt
+
 
 class Processor():
     def __init__(self, topic_name="", folder_path="./WikiPageDocument/", file_extension=".txt"):
@@ -10,6 +12,17 @@ class Processor():
             content = file.readlines()
             content = [ct for ct in content if ct != "\n"]
             tokenizer = nltk.tokenize.word_tokenize
-            content_matrix = [[tokenizer(line)] for line in content]
+            content_matrix = [tokenizer(line) for line in content]
             print(content_matrix[0])
+            
+            total_sent = []
+            for n,sent in enumerate(content_matrix):
+                for words in sent:
+                    total_sent.append(words.lower())
+                
+            fdist = nltk.probability.FreqDist(total_sent) 
+            print(fdist.most_common(15))
+            # fdist.plot()
+            # plt.show()
+            # TODO: REMOVE STOPWORDS!!!!
         file.close()
